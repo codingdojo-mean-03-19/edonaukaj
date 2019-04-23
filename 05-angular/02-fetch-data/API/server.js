83365f4d230135ce061a27e5acfbb33551796b6d
Edona Ukaj <edona.ukaj@hotmail.com>
2019-04-23 03:40:13 +0200

    API

---
+++ 05-angular/02-fetch-data/API/server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8000;
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'API/dist/API')));
+app.listen(port, () => console.log(`Listening on port ${port}`));
